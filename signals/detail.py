1fb5af3a04a314868909c23a4baea22f69f167b0
ShaneFlandermeyer <shaneflandermeyer@gmail.com>
2021-11-02 18:50:05 -0500

    Cleaned up detail.dict()

--- signals/detail.py
+++ signals/detail.py
@@ -3,7 +3,7 @@ class detail():
     """
     DETAIL_KEY = "signal:detail"
     TYPE_KEY = "type"
-    MODULATION_KEY = "class"
+    CLASS_KEY = "class"
     CARRIER_VARIANT_KEY = "carrier_variant"
     SYMBOL_VARIANT_KEY = "symbol_variant"
     ORDER_KEY = "order"
@@ -31,22 +31,11 @@ class detail():
     def dict(self):
         """
         Convert the supplied metadata to a dictionary of dictionaries
-        TODO: There's probably a smarter way to do this
         """
-        d = {self.DETAIL_KEY:[]}
-        d[self.DETAIL_KEY].append({self.TYPE_KEY:self.type})
-        d[self.DETAIL_KEY].append({self.MODULATION_KEY:self.modulation})
-        d[self.DETAIL_KEY].append({self.CARRIER_VARIANT_KEY:self.carrier_variant})
-        d[self.DETAIL_KEY].append({self.SYMBOL_VARIANT_KEY:self.symbol_variant})
-        d[self.DETAIL_KEY].append({self.ORDER_KEY:self.order})
-        d[self.DETAIL_KEY].append({self.DUPLEXING_KEY:self.duplexing})
-        d[self.DETAIL_KEY].append({self.MULTIPLEXING_KEY:self.multiplexing})
-        d[self.DETAIL_KEY].append({self.MULTIPLE_ACCESS_KEY:self.multiple_access})
-        d[self.DETAIL_KEY].append({self.SPREADING_KEY:self.spreading})
-        d[self.DETAIL_KEY].append({self.BANDIWDTH_KEY:self.bandwidth})
-        d[self.DETAIL_KEY].append({self.CHANNEL_KEY:self.channel})
-        d[self.DETAIL_KEY].append({self.CLASS_VARIANT_KEY:self.class_variant})
-
+        d = {self.DETAIL_KEY:vars(self)}
+        # Had to make 'modulation' the class variable storing the signal class
+        # (analog/digital) because class is a keyword in python
+        d[self.DETAIL_KEY][self.CLASS_KEY] = d[self.DETAIL_KEY].pop("modulation")
         return d
 
 if __name__ == '__main__':
